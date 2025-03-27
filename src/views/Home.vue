e20df2a20a75be5929d7084fc8b32b7a34f41d95
bjwashburn <bwashburn@eagles.bridgewater.edu>
2025-03-26 22:52:58 -0400

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+</script>
+
+<template>
+    <main>
+        Hello World!
+    </main>
+</template>
+
+<style scoped>
+</style>
